c26c585c987e264ba2d1290dba243461e6afd11d
Sally Diament <sally.barzilai@gmail.com>
2022-05-01 19:37:57 -0300

    cumprindo quase todos os requisitos obrigatórios com exceção do 8

--- script.js
+++ script.js
@@ -19,12 +19,13 @@ botaoCriarTarefa.addEventListener('click', adicionaTarefa);
 // se eu chegar no requisito 12, eu uso o localstorage e a funcao criaLI.
 
 // Deixar com a cor de fundo cinza o item da lista que for clicado:
-// const itensDaLista = document.getElementsByTagName('li');
+const itensDaLista = document.getElementsByTagName('li');
 
 // listaToDo.addEventListener('click', function() {
-//      // for (let i = 0; i < listaToDo.childElementCount; i += 1) {
-//     // if (itensDaLista[i].classList.contains('classGray') === true) {
-//     //     itensDaLista[i].classList.remove('classGray');
+  
+    // if (itensDaLista[i].classList.contains('classGray') === true) {
+      for (let i = 0; i < listaToDo.childElementCount; i += 1) {
+        itensDaLista[i].classList.remove('classGray')}
 //     //   }}
 //     let itemClicado = event.target;
 //     console.log(itemClicado);
@@ -33,7 +34,7 @@ botaoCriarTarefa.addEventListener('click', adicionaTarefa);
 listaToDo.addEventListener('click', function bkgdGray(event){
   let itemClicado = event.target;
   if (itemClicado.classList.contains('classGray') === false) {
-    itemClicado.classList.add('classGray')
+    itemClicado.classList.add('classGray');
   } else if (itemClicado.classList.contains('classGray') === true) {
     itemClicado.classList.remove('classGray')
   }
@@ -48,3 +49,29 @@ if (itemClicado.classList.contains('completed') === false) {
   itemClicado.classList.remove('completed')
 }
 });
+
+// Limpar lista:
+// Ref: https://cursos.alura.com.br/forum/topico-excluir-todos-os-elementos-com-uma-classe-159597
+function apagar() {
+  const lista = document.getElementsByTagName('li');
+  for (let i = lista.length - 1; i >= 0; i -= 1) {
+    lista[i].remove();
+  }
+}
+
+const botaoLimparLista = document.getElementById('apaga-tudo');
+
+botaoLimparLista.addEventListener('click', apagar);
+
+// Limpar itens completos da lista:
+function removeRiscados() {
+  const lista = document.getElementsByTagName('li');
+  for (let i = lista.length - 1; i >= 0; i -= 1) {
+    if (lista[i].classList.contains('completed') === true) {
+      lista[i].remove();
+    }
+  }
+}
+
+const botaoLimparItensCompletos = document.getElementById('remover-finalizados');
+botaoLimparItensCompletos.addEventListener('click', removeRiscados)
