080cf565bc196581a78a758ddd0da1baab75779b
Sally Diament <sally.barzilai@gmail.com>
2022-11-03 14:29:53 -0300

    incluindo últimas mudanças

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function adicionaTarefa() {
   const inputTarefa = document.getElementById('texto-tarefa');
   tarefas = inputTarefa.value;
   criaLi();
-  localStorage.setItem(tarefas, JSON.stringify(tarefas));
   inputTarefa.value = '';
 }
 botaoCriarTarefa.addEventListener('click', adicionaTarefa);
@@ -26,35 +25,19 @@ function removeClassGray() {
   }
 }
 
-// });
-listaToDo.addEventListener('click', function bkgdGray(event){
- removeClassGray();
-  let itemClicado = event.target;
-  itemClicado.classList.add('classGray');
-  });
-
-// Deixar item da lista que for clicado riscado:
-listaToDo.addEventListener('dblclick', function riscarPalavra(event){
-let itemClicado = event.target;
-if (itemClicado.classList.contains('completed') === false) {
-  itemClicado.classList.add('completed')
-} else if (itemClicado.classList.contains('completed') === true) {
-  itemClicado.classList.remove('completed')
-}
+listaToDo.addEventListener('click', (event) => {
+  removeClassGray();
+  event.target.classList.add('classGray');
 });
 
-// Limpar lista:
-// Ref: https://cursos.alura.com.br/forum/topico-excluir-todos-os-elementos-com-uma-classe-159597
-function apagar() {
-  const lista = document.getElementsByTagName('li');
-  for (let i = lista.length - 1; i >= 0; i -= 1) {
-    lista[i].remove();
+// Deixar item da lista que for clicado riscado:
+listaToDo.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed') === false) {
+    event.target.classList.add('completed');
+  } else {
+    event.target.classList.remove('completed');
   }
-}
-
-const botaoLimparLista = document.getElementById('apaga-tudo');
-
-botaoLimparLista.addEventListener('click', apagar);
+});
 
 // Limpar itens completos da lista:
 function removeRiscados() {
@@ -70,14 +53,42 @@ const botaoLimparItensCompletos = document.getElementById('remover-finalizados')
 botaoLimparItensCompletos.addEventListener('click', removeRiscados);
 
 // Adicionando o botão salvar tarefas:
+
+function salvar() {
+  const arrayItensLista = [];
+  const lista = document.getElementsByTagName('li');
+  for (let i = 0; i < lista.length; i += 1) {
+    arrayItensLista.push(lista[i].innerText);
+  }
+  localStorage.setItem('listaToDo', JSON.stringify(arrayItensLista));
+}
+
+const botaoSalvarTarefas = document.getElementById('salvar-tarefas');
+botaoSalvarTarefas.addEventListener('click', salvar);
+
 function resgatarSalvos() {
-  if (localStorage.length > 0) {
-    for (let i = 0; i < localStorage.length; i += 1) {
-      const chave = localStorage.key(i);
-      tarefas = JSON.parse(localStorage[chave]);
-      criaLi();
-    }
+  tarefas = JSON.parse(localStorage.getItem('listaToDo'));
+  for (let i = 0; i < tarefas.length; i += 1) {
+    const novoItemLista = document.createElement('li');
+    novoItemLista.innerText = tarefas[i];
+    listaToDo.appendChild(novoItemLista);
   }
 }
-const botaoSalvarTarefas = document.getElementById('salvar-tarefas');
-botaoSalvarTarefas.addEventListener('click', resgatarSalvos);
+
+window.onload = () => {
+  resgatarSalvos();
+};
+
+// Limpar lista:
+// Ref: https://cursos.alura.com.br/forum/topico-excluir-todos-os-elementos-com-uma-classe-159597
+function apagar() {
+  localStorage.removeItem('listaToDo');
+  const lista = document.getElementsByTagName('li');
+  for (let i = lista.length - 1; i >= 0; i -= 1) {
+    lista[i].remove();
+  }
+}
+
+const botaoLimparLista = document.getElementById('apaga-tudo');
+
+botaoLimparLista.addEventListener('click', apagar);
